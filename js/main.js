abbb640567cf8c6a4199453bcfb13de313cddee8
Роман <gaerroma28@gmail.com>
2024-07-20 13:08:33 +0300

    two save

--- js/main.js
+++ js/main.js
@@ -2,11 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const burgerBtn = document.querySelector('.burger-menu--btn');
     const nav = document.querySelector('.header__menu--nav');
     const links = document.querySelector('.header__menu--links')
+    const menu_link = document.querySelector('.menu-link')
 
     burgerBtn.addEventListener('click', function (e) {
         e.preventDefault();
         burgerBtn.classList.toggle('burger-active');
         nav.classList.toggle('menu-active');
-        links.classList.toggle('menu-active');
+        links.classList.toggle('links-active');
     });
+
+    menu_link.addEventListener('click', function (e) {
+        e.preventDefault();
+        burgerBtn.classList.remove('burger-active');
+        nav.classList.remove('menu-active');
+        links.classList.remove('links-active');
+    })
 });
